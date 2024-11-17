95e447c0dc5591c7bbc11e88a4d90fc45b1e0258
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2024-11-17 13:52:13 -0300

    Update 2_turning_on_a_led_with_button.c

--- 2_turning_on_a_led_with_button.c
+++ 2_turning_on_a_led_with_button.c
@@ -3,19 +3,19 @@
 void main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // para watchdog timer
-    P1DIR = 0b00000001;         // definindo P1.0 como saída e P1.1 como entrada
-    P1REN |= 0b00000010;        // habilitando pull-up e pull down e P1.1
-    P1OUT |= 0b00000010;        // escolhendo pull-up em P1.1
+    P1DIR = BIT0;               // definindo P1.0 como saída e P1.1 como entrada
+    P1REN |= BIT1;              // habilitando pull-up e pull-down eM P1.1
+    P1OUT |= BIT1;              // escolhendo pull-up em P1.1
 
     while(1)
     {
-     if((P1IN & 0b00000010) == 0)
+     if((P1IN & BIT1) == 0)
      {
-         P1OUT |= 0b00000001;
+         P1OUT |= BIT0;         // acende o led em P1.0
      }
      else
      {
-         P1OUT &= ~0b00000001;
+         P1OUT &= ~BIT0;        // apaga o led em P1.0
      }
     }
     return 0;
